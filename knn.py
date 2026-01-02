56ba7af1ae6c2f8da7eba1dd58197440d93e1016
Afreh krissNtim <afrehkriss2101@gmail.com>
2026-01-02 16:02:40 +0000

    completed knn

--- knn.py
+++ knn.py
@@ -1,9 +1,33 @@
+import numpy as np
+from collections import Counter
+
+
+def euclidean_distance(x1,x2):
+    return np.sqrt(np.sum((x1 - x2)**2))
+
+
+
 class KNN:
     def __init__(self,k=3):
         self.k = k
 
     def fit(self, X,y):
-        pass
+        self.X_train = X
+        self.y_train = y
 
     def predict(self,X):
-        pass
+        predictated_labels = [self._predict(x) for x in X]
+        return np.array(predictated_labels)
+    
+    def _predict(self,x):
+        # comput distances
+        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+
+        # get K nearest sample,label
+        k_indics = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indics]
+
+        #majority vote, most common class label
+        most_common = Counter(k_nearest_labels).most_common(1)
+        return most_common[0][0]
+    
--- knntest.py
+++ knntest.py
@@ -10,11 +10,20 @@ X,y = iris.data, iris.target
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2, random_state=1234)
 
-print(X_test.shape)
+# print(X_test.shape)
 
-plt.figure(figsize=(10,6))
-plt.scatter(X_test[:,0],X_test[:,1], c=y_test, cmap=cmap, edgecolor='k', s=100 )
-plt.xlabel(iris.feature_names[0])
-plt.ylabel(iris.feature_names[1])
-plt.title("Test Data Distribution")
-plt.show()
+# plt.figure(figsize=(10,6))
+# plt.scatter(X_test[:,0],X_test[:,1], c=y_test, cmap=cmap, edgecolor='k', s=20 )
+# plt.xlabel(iris.feature_names[0])
+# plt.ylabel(iris.feature_names[1])
+# plt.title("Test Data Distribution")
+# plt.show()
+
+from knn import KNN
+
+test1 = KNN(k=7)
+test1.fit(X_train,y_train)
+predictions = test1.predict(X_test)
+
+acc = np.sum(predictions == y_test) / len(y_test)
+print(acc)
